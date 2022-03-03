734d309a70238a7a114af7559afec04c7f244a09
Hudson-Pereira <hudson.o.pereira@gmail.com>
2022-03-03 17:02:43 -0300

    fornecedor OK

--- src/fornecedor/fornecedor.service.ts
+++ src/fornecedor/fornecedor.service.ts
@@ -1,26 +1,90 @@
-import { Injectable } from '@nestjs/common';
-import { CreateFornecedorDto } from './dto/create-fornecedor.dto';
-import { UpdateFornecedorDto } from './dto/update-fornecedor.dto';
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { Fornecedor, Prisma } from "@prisma/client";
+import { PrismaService } from "src/prisma/prisma.service";
+import { CreateFornecedorDto } from "./dto/create-fornecedor.dto";
+import { UpdateFornecedorDto } from "./dto/update-fornecedor.dto";
 
 @Injectable()
 export class FornecedorService {
-  create(createFornecedorDto: CreateFornecedorDto) {
-    return 'This action adds a new fornecedor';
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: Prisma.FornecedorCreateInput): Promise<Fornecedor> {
+    try {
+      const fornecedor = await this.prisma.fornecedor.create({ data });
+
+      console.log(`Produto ${fornecedor.nome} criado com sucesso.`);
+      return fornecedor;
+    } catch (error) {
+      console.error(error);
+      throw new HttpException("ERRO", HttpStatus.BAD_REQUEST);
+    }
   }
 
-  findAll() {
-    return `This action returns all fornecedor`;
+  async findAll(): Promise<Fornecedor[]> {
+    try {
+      const total = await this.prisma.fornecedor.findMany();
+      console.log(`Temos ${total.length} fornecedores.`);
+      if (!total) {
+        console.log("Nenhum item encontrado.");
+        throw new HttpException("Nenhum item encontrado", HttpStatus.NOT_FOUND);
+      }
+      return total;
+    } catch (error) {
+      console.error(error);
+      throw new HttpException("ERRO", HttpStatus.BAD_REQUEST);
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} fornecedor`;
+  async findOne(id: number): Promise<Fornecedor> {
+    try {
+      const fornecedor = await this.prisma.fornecedor.findUnique({
+        where: { id },
+      });
+      if (!fornecedor) {
+        console.log("Nenhum item encontrado.");
+        throw new HttpException("Nenhum item encontrado", HttpStatus.NOT_FOUND);
+      }
+      return fornecedor;
+    } catch (error) {
+      console.error(error);
+      throw new HttpException("ERRO", HttpStatus.BAD_REQUEST);
+    }
   }
 
-  update(id: number, updateFornecedorDto: UpdateFornecedorDto) {
-    return `This action updates a #${id} fornecedor`;
+  async update(
+    id: number,
+    _updateFornecedorDto: UpdateFornecedorDto
+  ): Promise<Fornecedor> {
+    try {
+      const fornecedor = await this.prisma.fornecedor.update({
+        data: { ..._updateFornecedorDto },
+        where: { id },
+      });
+      if (!fornecedor) {
+        console.log("Nenhum item encontrado.");
+        throw new HttpException("Nenhum item encontrado", HttpStatus.NOT_FOUND);
+      }
+      return fornecedor;
+    } catch (error) {
+      console.error(error);
+      throw new HttpException("ERRO", HttpStatus.BAD_REQUEST);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} fornecedor`;
+  async remove(id: number) {
+    try {
+      const fornecedor = await this.prisma.fornecedor.delete({ where: { id } });
+      if (!fornecedor) {
+        console.log("Nenhum item encontrado.");
+        throw new HttpException("Nenhum item encontrado", HttpStatus.NOT_FOUND);
+      }
+      return fornecedor;
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(
+        "Erro ao excluir, tente novamente",
+        HttpStatus.NOT_FOUND
+      );
+    }
   }
 }
